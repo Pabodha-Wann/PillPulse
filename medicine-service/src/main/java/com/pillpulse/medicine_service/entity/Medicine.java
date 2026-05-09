58ad3b2847661a44e2ad7e5320d353a44a8e11e2
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-05-10 04:03:39 +0530

    feat(medicine): create Medicine entity model

---
+++ medicine-service/src/main/java/com/pillpulse/medicine_service/entity/Medicine.java
@@ -0,0 +1,50 @@
+package com.pillpulse.medicine_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="medicines")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Medicine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "generic_name")
+    private String genericName;
+
+    private String category;
+    private String description;
+    private String manufacturer;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
+}
