e0eb79f6c3ac9ee58bb0dc9e62a6b13c9d18aecc
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-05-22 10:27:41 +0530

    feat: create axios instance with auth interceptors for token injection

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -1,3 +1,4 @@
+import { useAuthStore } from "@/store/authStore"
 import axios from "axios"
 
 const axiosInstance = axios.create({
@@ -8,3 +9,29 @@ const axiosInstance = axios.create({
     }
 })
 
+//request instance
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = useAuthStore.getState().token
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
+)
+
+
+//respose inceptor
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            useAuthStore.getState().logout()
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default axiosInstance
