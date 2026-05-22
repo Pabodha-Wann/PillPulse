7a4b17497ababb75ea786c372312536d946dd6aa
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-05-22 12:01:24 +0530

    feat: implement authService for pharmacy registration, login, and profile management

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,59 @@
+import axiosInstance from "@/lib/axiosInstance"
+import { Pharmacy } from "@/lib/types"
+
+export const authService = {
+    registerPharmacy: async (data: {
+        name: string
+        email: string
+        password: string
+        address: string
+        latitude: number
+        longitude: number
+        phone: string
+    }): Promise<Pharmacy> => {
+        const response = await axiosInstance.post('/api/register/register', data)
+        return response.data
+    },
+
+
+    loginPharamcy: async (email: string, password: string) => {
+
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_KEYCLOAK_URL}/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/protocol/openid-connect/token`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams({
+                    username: email,
+                    password: password,
+                    client_id: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID!,
+                    client_secret: process.env.KEYCLOAK_CLIENT_SECRET!,
+                    grant_type: 'password',
+                }).toString(),
+            }
+        )
+
+
+        if (!response.ok) {
+            throw new Error('Login failed')
+        }
+
+        return response.json()
+    },
+
+    // Get pharmacy details
+    getPharmacy: async (id: number): Promise<Pharmacy> => {
+        const response = await axiosInstance.get(`/api/pharmacies/${id}`)
+        return response.data
+    },
+
+    //update pharmacy
+    updatePharmacy: async (id: number, data: Partial<Pharmacy>): Promise<Pharmacy> => {
+        const response = await axiosInstance.put(`/api/pharmacies/${id}`, data)
+        return response.data
+    },
+
+
+}
